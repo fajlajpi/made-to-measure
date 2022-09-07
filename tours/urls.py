d384f86097f0075401c01b56b52a9fb68b877177
fajlajpi <fajlajpi@gmail.com>
2022-09-07 22:50:55 +0200

    Added URLs, Views and Templates
    Added basic templates (base, list, detail). Registered basic URLs. Created
    the basic Views.

---
+++ tours/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.tours_list, name='tours_list'),
+    path('tour/<int:pk>', views.tour_detail, name='tour_detail'),
+    path('test', views.test_view, name='test_view')
+]
--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+
+from .models import Tour
 
 # Create your views here.
+
+def tours_list(request):
+    tours = Tour.objects.all().order_by('name')
+    return render(request, 'tours/tours_list.html', {'tours': tours})
+
+def tour_detail(request, pk):
+    tour = get_object_or_404(Tour, pk=pk)
+    return render(request, 'tours/tour_detail.html', {'tour': tour})
+
+def test_view(request):
+    html = "<html><body><h1>TEST</h1></body></html>"
+    return HttpResponse(html)
